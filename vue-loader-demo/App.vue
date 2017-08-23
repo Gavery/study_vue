ed4ab67a015946b5383d39242ae317198c9ab94c
Gavery <2294625664@qq.com>
2017-08-23 22:47:27 +0800

    vue-loader

---
+++ vue-loader-demo/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<h1 @click="change">welcome vue</h1>
+	<h2>{{msg}}</h2>
+	<my_menu></my_menu>
+</template>
+<script type="text/javascript">
+	import Menu from "./components/menu.vue";
+	export default{
+		data(){
+			return {
+				msg:'welcome vue!!'
+			}
+		},
+		methods:{
+			change(){
+				this.msg="hello vue"
+			}
+		},
+		components:{
+			"my_menu":Menu
+		}
+	}
+</script>
+<style type="text/css">
+	body{
+		background: #ccc;
+	}
+</style>
